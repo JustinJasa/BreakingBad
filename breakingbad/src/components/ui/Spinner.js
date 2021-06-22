b267427c32f2fca03a1b25d57513a87be2dbd49d
JustinJasa <jjasa.2000@gmail.com>
2021-06-22 23:04:44 +1000

    first commit

---
+++ breakingbad/src/components/ui/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import spinner from '../../img/spinner.gif'
+
+const Spinner = () => {
+  return (
+    <img className = 'spinner' src={spinner} alt="loading" />
+  )
+}
+
+export default Spinner
